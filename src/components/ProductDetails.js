17cc3eb3ef2c2c6126ee07f9b48b0b9bc29fc4bf
kingmoc <joe.c.a.garcia@gmail.com>
2020-02-26 13:34:34 -0600

    Add Cart Error message Logic Complete

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Commerce from '@chec/commerce.js'
 import ImageGallery from 'react-image-gallery';
-import { Header, Button, Dropdown } from 'semantic-ui-react';
+import { Header, Button, Dropdown, Label } from 'semantic-ui-react';
 
 import {numOfShirts} from '../misc/quanityOptions'
 
@@ -13,8 +13,8 @@ const ProductDetails = (props) => {
 
     const [product, setProduct] = useState([])
     const [images, setImages] = useState([])
-    const [numShirts, setNumShirts] = useState()
-    const [variantInfo, setVariantInfo] = useState()
+    const [numShirts, setNumShirts] = useState(1)
+    const [variantInfo, setVariantInfo] = useState(1)
 
     let productId = props.match.params.id
 
@@ -74,46 +74,53 @@ const ProductDetails = (props) => {
 
     const clearActive = () => {
         let btn = document.querySelectorAll('.button-sizes')
-        // console.log(btn, 'buttons')
+
         btn.forEach(el => {
-            console.log(el.className, 'class list')
             el.classList.remove('active')
         })
     }
 
-    const handleClick = e => {
+    const handleVariantInfo = e => {
 
         const variantId = e.target.id
         const variantOption = e.target.value
 
         if (!e.target.classList.contains('active')) {
-            console.log('inside if')
             setVariantInfo({[variantId]: variantOption})
             clearActive()
         }
 
         if (e.target.classList.contains('active')) {
-            console.log('in side second if')
             setVariantInfo(null)
         }
         
         e.target.classList.toggle('active')
-    
     }
 
-    const getSelectedInput = (e, {value}) => {
-        console.log(value, 'value')
+    const getQuanity = (e, {value}) => {
         setNumShirts(value)
     }
 
     const addToCart = e => {
-        e.preventDefault()
-        commerce.cart.add(product.id, numShirts, variantInfo)
-            .then(res => {
-                console.log(res, 'response from adding to Cart')
-                props.setCartVisible(true)
-                props.setCartQaunity(props.cartQuanity + 1)
-            })
+        // e.preventDefault()
+        // commerce.cart.add(product.id, numShirts, variantInfo)
+        //     .then(res => {
+        //         console.log(res, 'response from adding to Cart')
+        //         props.setCartVisible(true)
+        //         props.setCartQaunity(props.cartQuanity + 1)
+        //     })
+
+        if (variantInfo !== 1) {
+            console.log('add to cart!')
+            clearActive()
+        } else {
+            setVariantInfo(false)
+        }
+
+
+        console.log(product.id, 'product ID')
+        console.log(numShirts, 'quanity of shirt')
+        console.log(variantInfo, 'variant information')
     }
 
     return (
@@ -122,32 +129,54 @@ const ProductDetails = (props) => {
             <Header>{product.name}</Header>
             {product.length !==0 && (
                 <>
-                    <Header>{product.price.formatted_with_symbol}</Header>
-                    <Dropdown
-                        onChange={getSelectedInput} 
-                        // value={numShirts}
-                        fluid
-                        placeholder='How Many' 
-                        selection
-                        options={numOfShirts}
-                    />
-                    {product.variants[0].options.map(size => {
-                        return(
-                            <Button 
-                                className='button-sizes'
-                                basic 
-                                key={size.id}
-                                onClick={handleClick}
-                                value={size.id}
-                                id={product.variants[0].id}
-                            >
-                                {size.name}
-                            </Button>
-                        )
-                    })}
+                    <div className='price-quanity'>
+                        <Dropdown
+                            className="quanity-drop"
+                            onChange={getQuanity}
+                            value={numShirts} 
+                            fluid
+                            placeholder='How Many' 
+                            selection
+                            options={numOfShirts}
+                        />
+                        <Header>{product.price.formatted_with_symbol}</Header>
+                    </div>
+
+                    <div className='all-btn-sizes'>
+                        {product.variants[0].options.map(size => {
+                            return(
+                                <Button 
+                                    className='button-sizes'
+                                    basic 
+                                    key={size.id}
+                                    onClick={handleVariantInfo}
+                                    value={size.id}
+                                    id={product.variants[0].id}
+                                >
+                                    {size.name}
+                                </Button>
+                            )
+                        })}
+                    </div>
+                    {!variantInfo && (
+                        <Label
+                            className='label-sizes' 
+                            basic 
+                            color='red' 
+                        >
+                            Please select size
+                        </Label>
+                    )} 
                 </>
             )}
-            <Button onClick={addToCart} size='large' color='green'>Add to Cart</Button>
+            <Button 
+                onClick={addToCart}
+                fluid 
+                size='big' 
+                color='green'
+            >
+                Add to Cart
+            </Button>
         </>
     );      
 };
